695fdda736d7dc2070678dadbe15a6c2a255f436
Alan <alan0613@live.fr>
2023-06-27 16:35:15 +0200

    premiere requete bdd

--- app.js
+++ app.js
@@ -1,26 +1,74 @@
-const express = require('express');
-
+const express = require("express");
+const mysql = require("mysql");
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('votre reponse next');
-    next();
+// Configuration de la connexion à la base de données
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "alankalkan",
+  database: "marquise",
+});
+//Connection a la base de donnée
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connecté à la base de données MySQL!");
 });
 
-app.use((req,res,next)=>{
-    res.status(201);
-    next();
+connection.query(
+  "SELECT utilisateurs.id as 'utilisateurs_id', utilisateurs.nom as 'utilisateurs_nom', utilisateurs.prenom as 'utilisteur_prenom' FROM utilisateurs",
+  function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+    // Vous pouvez faire quelque chose avec les résultats ici
+  }
+);
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
+// app.post("/api/stuff", (req, res, next) => {
+//   console.log(req.body);
+//   res.status(201).json({
+//     message: "objet créé",
+//   });
+// });
 
-app.use((req,res,next)=>{
-    res.json({message:'votre groupe'});
-next();
+app.get("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      nom: "Ernser",
+      email: "Jennie.Koss50@yahoo.com",
+      mdp: "Vj7khbwcV3wClRB",
+      identifiant: "1",
+    },
+    {
+      nom: "Hansen",
+      email: "Viva_Hackett@hotmail.com",
+      mdp: "eHYhpZSNYZvYhXC",
+      identifiant: "2",
+    },
+    {
+      nom: "Brown",
+      email: "Darrion92@yahoo.com",
+      mdp: "GCZRZ63zAQBbRVk",
+      identifiant: "3",
+    },
+  ];
+
+  res.status(200).json(stuff);
 });
-app.use((req,res)=>{
-    console.log('votre reponse envoyee avec succes');
-
-});
-
 
 module.exports = app;
--- readme.md
+++ readme.md
@@ -1,2 +1,15 @@
 Installation de Express
 Installation de MySQL
+
+Importation des modules express et mysql.
+Création d'une instance d'Express appelée app.
+Configuration de la connexion à la base de données MySQL en utilisant les informations de l'hôte, de l'utilisateur et du mot de passe.
+Établissement de la connexion à la base de données en utilisant connection.connect().
+Utilisation du middleware express.json() pour analyser les données JSON envoyées dans le corps des requêtes.
+Utilisation d'un middleware pour gérer les en-têtes CORS afin d'autoriser les requêtes provenant de différents domaines.
+Définition d'une route GET /api/stuff qui renvoie une liste statique d'objets stuff au format JSON.
+Export de l'instance d'Express (app) pour être utilisée dans d'autres fichiers.
+
+Cela crée un serveur qui écoute sur le port par défaut (3000) et répond aux requêtes GET sur l'endpoint /api/stuff en renvoyant la liste d'objets stuff.
+
+Veuillez noter que la connexion à la base de données est établie, mais aucune requête SQL n'est effectuée dans cet exemple. Vous devrez ajouter des routes supplémentaires et des requêtes SQL appropriées pour interagir avec votre base de données MySQL en fonction de vos besoins spécifiques.
