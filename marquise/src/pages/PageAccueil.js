7bdd0fc1fc22fc799a80670ff62b7103ab2eec08
b-khadija <khadija.bayssir@gmail.com>
2023-06-29 20:40:09 +0200

    Ajout du header et du footer dynamique avec Layout.js

--- marquise/src/pages/PageAccueil.js
+++ marquise/src/pages/PageAccueil.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 
 const PageAccueil = () => {
     return (
         <div>
-            <Header />
             <h1>coucou</h1>
-            <Footer />
         </div>
     );
 };
