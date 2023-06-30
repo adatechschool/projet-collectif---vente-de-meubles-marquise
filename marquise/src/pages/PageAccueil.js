ec0fed3894f0acf942f088ad54719a9f70262e9a
Mafoa <steve_mafoa@outlook.com>
2023-06-30 14:25:20 +0200

    Ajout de la section collections dans la page d'acceuil

--- marquise/src/pages/PageAccueil.js
+++ marquise/src/pages/PageAccueil.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import canape from "../assets/livingRoom.jpg";
+import chaise from "../assets/categorieChaise.jpg";
+import fauteuil from "../assets/categorieFauteuil.jpg";
+import armoire from "../assets/categorieArmoire.jpg";
 
 const PageAccueil = () => {
     return (
         <div>
-            <h1>coucou</h1>
+            <h1 className='font-bold ml-1.5 mt-6'>COLLECTIONS</h1>
+            <div className='flex justify-around gap-2 p-2 text-center w-full h-full mb-14'>
+                <div className='w-1/4 h-48 rounded-lg'><img src={canape} className="w-full h-full rounded-lg" alt="Photo categorie Canapé" /><h2 className='relative bottom-8 left-18 text-white'>Canapé</h2></div>
+                <div className='w-1/4 h-48 rounded-lg'><img src={chaise} className="w-full h-full rounded-lg" alt="Photo categorie Chaise" /><h2 className='relative left-26 bottom-8 text-white'>Chaise</h2></div>
+                <div className='w-1/4 h-48 rounded-lg'><img src={fauteuil} className="w-full h-full rounded-lg" alt="Photo categorie Fauteuil" /><h2 className='relative bottom-8 left-34 text-white'>Fauteuil</h2></div>
+                <div className='w-1/4 h-48 rounded-lg'><img src={armoire} className="w-full h-full rounded-lg" alt="Photo categorie Armoire" /><h2 className='relative bottom-8 left-42 text-white'>Armoire</h2></div>
+            </div>
         </div>
+        
     );
 };
 
