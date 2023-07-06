7f8dcfdaf341d1b05564739d5e15e05e9ed5901a
Alan <alan0613@live.fr>
2023-07-06 10:01:31 +0200

    panier

--- Backend/serveur.js
+++ Backend/serveur.js
@@ -2,7 +2,6 @@ const http = require("http");
 
 const app = require("./app");
 
-
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
   if (isNaN(port)) {
@@ -13,7 +12,7 @@ const normalizePort = (val) => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT || "3001");
 app.set("port", port);
 const errorHandler = (error) => {
   if (error.syscall !== "listen") {
